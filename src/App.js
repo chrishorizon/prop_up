235dc41049fe0411315ba5d1847223a48d741096
Chris <christhehorizon@gmail.com>
2021-09-02 19:40:38 -0700

    Putting it together assign.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import './App.css';
 import PersonalCard from './components/PersonalCard';
 
+var namesArr =[
+  {"userName":"Doe, Jane", "age":45, "hairColor":"Black"},
+  {"userName":"Smith, John","age":88,"hairColor":"Brown"},
+  {"userName":"Fillmore, Millard","age":50,"hairColor":"Brown"},
+  {"userName":"Smith, Maria","age":62,"hairColor":"Brown"}
+]
+
 function App() {
   return (
     <div className="App">
-      <PersonalCard userName={'Doe, Jane'}/>
-      <PersonalCard userAge={'Age: 45'}/>
-      <PersonalCard userHair={'Hair Color: Black'}/>
-      <PersonalCard userName={'Smith, John'}/>
-      <PersonalCard userAge={'Age: 88'}/>
-      <PersonalCard userHair={'Hair Color: Brown'}/>
-      <PersonalCard userName={'Fillmore, Millard'}/>
-      <PersonalCard userAge={'Age: 50'}/>
-      <PersonalCard userHair={'Hair Color: Brown'}/>
-      <PersonalCard userName={'Smith, Maria'}/>
-      <PersonalCard userAge={'Age: 62'}/>
-      <PersonalCard userHair={'Hair Color: Brown'}/>
+      {namesArr.map(person => {
+        return <PersonalCard userName={person.userName} age={person.age} hairColor={person.hairColor} />
+      })
+      }
     </div>
   );
 }
--- src/components/PersonalCard.jsx
+++ src/components/PersonalCard.jsx
@@ -1,14 +1,25 @@
 import React, { Component } from 'react'
 
 export class PersonalCard extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            age: this.props.age
+        };
+    }
+    newAge = () => {
+        this.setState({ age: this.state.age + 1});
+    }
+    
     render() {
         // destructured prop
         // const { userName } = this.props
         return (
             <div>
                 <h1>{this.props.userName}</h1>
-                <p>{this.props.userAge}</p>
+                <p>Age: {this.state.age}</p>
                 <p>{this.props.userHair}</p>
+                <button onClick={this.newAge}>Birthday Button for {this.props.userName}</button>
             </div>
         )
     }
